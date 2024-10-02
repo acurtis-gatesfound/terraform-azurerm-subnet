cce58b39719c4ade9090e7864234d500b3ce54fb
Aaron Curtis <aaron.curtis@gatesfoundation.org>
2024-10-02 08:31:09 -0700

    revert

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "The name of the subnet"
-  value       = azurerm_subnet.snet.name
-}
-
-output "id" {
-  description = "The ID of the subnet"
-  value       = azurerm_subnet.snet.id
-}
